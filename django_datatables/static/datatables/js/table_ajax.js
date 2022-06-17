745fb5599c1b2a7ac5d38d7e58b9c8a3df6b22fc
residentcode <55086042+residentcode@users.noreply.github.com>
2022-06-17 01:33:25 +0100

    Add files via upload

--- django_datatables/static/datatables/js/table_ajax.js
+++ django_datatables/static/datatables/js/table_ajax.js
@@ -1,1 +1,28 @@
+$(document).ready(function () {
+    let path_url = window.location.pathname
+    let table = $('#table-orders').DataTable({
+        pagingType: 'full_numbers',
+        responsive: true,
+        processing: true,
+        serverSide: true,
+        ajax: {
+            url: path_url,
+            type: 'POST',
+            headers: {
+                'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value,
+            }
+        },
+        columnDefs: {},
+        searchDelay: 500,
+        lengthMenu: [10, 20, 25, 30, 50, 100],
+        columns: [
+            { data: 'first_name' },
+            { data: 'last_name' },
+            { data: 'email' },
+            { data: 'gender' },
+            { data: 'city' },
+        ],
 
+    });
+
+});
